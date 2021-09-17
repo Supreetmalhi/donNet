dd5e79ca62ac7cba6646247bfb5ab4ff36826f16
Supreetmalhi <malhisupreet40@gmail.com>
2021-09-18 02:59:58 +0530

    classMethodAssignment1 assignment program

---
+++ Project/ClassesandMethodsAssignment1/ClassesandMethodsAssignment1/Calc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesandMethodsAssignment1
+{
+    class Calc
+    {
+        public int add(int num1, int num2)
+
+        {
+
+            return num1 + num2;
+
+        }
+
+        public int add(double num1, double num2)
+
+        {
+
+            return Convert.ToInt32(num1 + num2);
+
+        }
+        public int Subtract(int num1, int num2)
+
+        {
+
+            return num1 - num2;
+
+        }
+
+        public int Multiple(string str)
+
+        {
+
+            int num = Convert.ToInt32(str);
+
+            return num * 2;
+
+        }
+    }
+}
---
+++ Project/ClassesandMethodsAssignment1/ClassesandMethodsAssignment1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassesandMethodsAssignment1
+
+{
+
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+
+            Calc c = new Calc();
+
+            Console.WriteLine("Integer Function is Called");
+
+            Console.WriteLine(c.add(10, 20));
+
+            Console.WriteLine("Decimal Function is Called");
+
+            Console.WriteLine(c.add(10.1, 20.11));
+
+            Console.WriteLine("String Function is Called");
+
+            Console.WriteLine(c.Multiple("100"));
+
+        }
+
+    }
+
+}
